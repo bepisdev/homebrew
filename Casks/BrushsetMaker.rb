6e8afbae1989a06638c96ecfc3d209306b6a9c76
Josh Burns <joshyburnss@gmail.com>
2025-11-28 19:46:44 +1100

    cask for BrushsetMaker

---
+++ Casks/BrushsetMaker.rb
@@ -0,0 +1,10 @@
+cask "brushsetmaker" do
+    version "0.2.3"
+    sha256 "bcfa56f6515b9c6479eaeba18f117a7e09de46ee60e6615a2bb6f52e89e2465f" 
+    url "https://github.com/bepisdev/brushsetmaker/releases/download/untagged-7c3e3a452c551499b828/BrushsetMaker-0.2.3.zip"
+    name "BrushsetMaker"
+    desc "Compiler for procreate bushset files"
+    homepage "https://bepisdev.github.io/brushsetmaker/"
+
+    app "BrushsetMaker.app"
+end
